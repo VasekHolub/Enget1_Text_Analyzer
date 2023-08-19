dbc4d8b141caa3b3e3c99827e3b9f79ad29d2817
VasekHolub <vholub33@gmail.com>
2023-08-19 16:53:57 +0200

    add login process

--- project_1.py
+++ project_1.py
@@ -4,3 +4,20 @@ author: Václav Holub
 email: vaclavholub5@seznam.cz
 discord: .avalok
 """
+import task_template as texts
+
+#Variables
+registered_users = {'bob': '123',
+                    'ann': 'pass123',
+                    'mike': 'password123',
+                    'liz': 'pass123'}
+sep = '-'*40
+while True:
+  user_name = input('Enter username: ')
+  user_password = input('Enter password: ')
+  login = {user_name: user_password}
+  if registered_users.get(user_name) == user_password:
+    print(f'{sep}\nWelcome to the app {user_name}\nWe have 3 texts to be analyzed.\n{sep}')
+  else:
+    print('unregistered user, terminating the program...')
+    break
