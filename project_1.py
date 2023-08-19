d6c01aeb55d07b1ccf079b1f7d96db1625078308
VasekHolub <vholub33@gmail.com>
2023-08-19 18:18:27 +0200

    Removed while loop Added word and upper case count

--- project_1.py
+++ project_1.py
@@ -12,24 +12,29 @@ registered_users = {'bob': '123',
                     'mike': 'password123',
                     'liz': 'pass123'}
 sep = '-'*40
+words = ''
+upper_case = []
 
-#Main loop
+#Login
+user_name = input('Enter username: ')
+user_password = input('Enter password: ')
+if registered_users.get(user_name) == user_password:
+  print(f'{sep}\nWelcome to the app {user_name}\nWe have 3 texts to be analyzed.\n{sep}')
+else:
+  print('unregistered user, terminating the program...')
 
-while True:
-  user_name = input('Enter username: ')
-  user_password = input('Enter password: ')
-  login = {user_name: user_password}
-  if registered_users.get(user_name) == user_password:
-    print(f'{sep}\nWelcome to the app {user_name}\nWe have 3 texts to be analyzed.\n{sep}')
+#Text selection
+text_choice = input('Enter a number btw. 1 and 3 to select: ')
+print(sep)
+if not text_choice.isnumeric() or int(text_choice) - 1 not in range(0,3):
+  print('Invalid text selection, terminating the program...')
+
+#Text analysis
+text_list = texts.TEXTS[int(text_choice) - 1].split()
+words = len(text_list)
+for i in text_list:
+  if i[0].isupper():
+    upper_case.append(i)
   else:
-    print('unregistered user, terminating the program...')
-    break
-  text_choice = input('Enter a number btw. 1 and 3 to select: ')
-  print(sep)
-  if not text_choice.isnumeric() or int(text_choice) - 1 not in range(0,3):
-    print('Invalid text selection, terminating the program...')
-    break
-  else:
-    print(texts.TEXTS[int(text_choice) - 1])
-    break
-  
+    continue
+print(upper_case)
