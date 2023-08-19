744e8ba5730ab380db545e5ac854671d7d788784
VasekHolub <vholub33@gmail.com>
2023-08-19 18:57:13 +0200

    Added lowercase, numbers and sum.

--- project_1.py
+++ project_1.py
@@ -4,6 +4,7 @@ author: Václav Holub
 email: vaclavholub5@seznam.cz
 discord: .avalok
 """
+import sys
 import task_template as texts
 
 #Variables
@@ -12,8 +13,11 @@ users = {'bob': '123',
           'mike': 'password123',
           'liz': 'pass123'}
 sep = '-' * 40
-upper_case_letter = []
+title_words = []
 upper_case_word = []
+lower_case_word = []
+numerics = []
+int_numerics = []
 
 #Login
 user_name = input('Enter username: ')
@@ -22,19 +26,21 @@ if users.get(user_name) == user_password:
   print(f'{sep}\nWelcome to the app {user_name}\nWe have 3 texts to be analyzed.\n{sep}')
 else:
   print('unregistered user, terminating the program...')
+  sys.exit()
 
 #Text selection
 text_choice = input('Enter a number btw. 1 and 3 to select: ')
 print(sep)
 if not text_choice.isnumeric() or int(text_choice) - 1 not in range(0,3):
   print('Invalid text selection, terminating the program...')
+  sys.exit()
 
 #Text analysis
 text_list = texts.TEXTS[int(text_choice) - 1].split()
 words = len(text_list)
 for i in text_list:
-  if i[0].isupper():
-    upper_case_letter.append(i)
+  if i.istitle():
+    title_words.append(i)
   else:
     continue
 for i in text_list:
@@ -42,5 +48,23 @@ for i in text_list:
     upper_case_word.append(i)
   else:
     continue
-uppper_case_letter_count = len(upper_case_letter)
+for i in text_list:
+  if not i.istitle() and not i.isnumeric() and not i.isupper():
+    lower_case_word.append(i)
+  else:
+    continue
+for i in text_list:
+  if i.isnumeric():
+    numerics.append(i)
+  else:
+    continue
+for i in numerics:
+  int_numerics.append(int(i))
+
+#Counts of occurrences
+title_words_count = len(title_words)
 upper_case_word_count = len(upper_case_word)
+lower_case_word_count = len(lower_case_word)
+numerics_count = len(numerics)
+numerics_sum = sum((int_numerics))
+
