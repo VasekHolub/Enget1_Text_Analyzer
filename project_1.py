3940044b78d10e0325731c89c9619986dfd9c2cf
VasekHolub <vholub33@gmail.com>
2023-11-19 11:24:48 +0100

    Changed input format of text removed it's line breaks for analysis

--- project_1.py
+++ project_1.py
@@ -8,9 +8,31 @@ import sys
 import re
 
 texts = [
-    "Situated about 10 miles west of Kemmerer, Fossil Butte is a ruggedly impressive topographic feature that rises sharply some 1000 feet above Twin Creek Valley to an elevation of more than 7500 feet above sea level. The butte is located just north of US 30N and the Union Pacific Railroad, which traverse the valley.",
-    "At the base of Fossil Butte are the bright red, purple, yellow and gray beds of the Wasatch Formation. Eroded portions of these horizontal beds slope gradually upward from the valley floor and steepen abruptly. Overlying them and extending to the top of the butte are the much steeper buff-to-white beds of the Green River Formation, which are about 300 feet thick.",
-    "The monument contains 8198 acres and protects a portion of the largest deposit of freshwater fish fossils in the world. The richest fossil fish deposits are found in multiple limestone layers, which lie some 100 feet below the top of the butte. The fossils represent several varieties of perch, as well as other freshwater genera and herring similar to those in modern oceans. Other fish such as paddlefish, garpike and stingray are also present.",
+    """Situated about 10 miles west of Kemmerer,
+Fossil Butte is a ruggedly impressive
+topographic feature that rises sharply
+some 1000 feet above Twin Creek Valley
+to an elevation of more than 7500 feet
+above sea level. The butte is located just
+north of US 30N and the Union Pacific Railroad,
+which traverse the valley. """,
+    """At the base of Fossil Butte are the bright
+red, purple, yellow and gray beds of the Wasatch
+Formation. Eroded portions of these horizontal
+beds slope gradually upward from the valley floor
+and steepen abruptly. Overlying them and extending
+to the top of the butte are the much steeper
+buff-to-white beds of the Green River Formation,
+which are about 300 feet thick.""",
+    """The monument contains 8198 acres and protects
+a portion of the largest deposit of freshwater fish
+fossils in the world. The richest fossil fish deposits
+are found in multiple limestone layers, which lie some
+100 feet below the top of the butte. The fossils
+represent several varieties of perch, as well as
+other freshwater genera and herring similar to those
+in modern oceans. Other fish such as paddlefish,
+garpike and stingray are also present.""",
 ]
 
 
@@ -41,7 +63,8 @@ def text_selection(texts: list) -> int:
 
 
 def text_nonalnum_char_strip(text_to_analyse: str) -> list:
-    return re.sub(r"[^a-zA-Z0-9- ]", "", text_to_analyse)
+    removed_lines = text_to_analyse.replace("\n", " ")
+    return re.sub(r"[^a-zA-Z0-9- ]", "", removed_lines)
 
 
 def text_analysis(striped_text: list) -> list:
@@ -115,6 +138,7 @@ def graph_printer(sorted_word_length: list) -> None:
 def main():
     user_login()
     striped_text = text_nonalnum_char_strip(texts[text_selection(texts) - 1])
+    print(striped_text)
     text_to_analyse = striped_text.split()
     analysed_text = text_analysis(text_to_analyse)
     sorted_word_length = sort_word_length(text_to_analyse)
